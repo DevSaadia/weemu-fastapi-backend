71696d2cebf0d53767357182709741d17dd8dc64
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-08-28 19:04:49 -0400

    feat: add main

---
+++ main.py
@@ -0,0 +1,37 @@
+from typing import Union
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+df = pd.read_csv('archive/movies_metadata.csv', low_memory=False)
+df = df[['title','overview']]
+
+
+
+# df = pd.read_csv('archive/movies_metadata.csv')
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/data/{query}")
+def query_data(query: str):
+    # Perform some operation using the dataset and the query
+    return df.head()
+    # result = df[df['column_name'].str.contains(query, na=False)]
+    # # Convert the result to a dictionary
+    # result_dict = result.to_dict(orient='records')
+    # return {"result": result_dict}
+
+@app.get("/check/{title}")
+def check(title:str):
+    overview = df.loc[df['title'] == title, 'overview'].values
+    if len(overview) > 0:
+        return overview[0]
+    else:
+        return "Title not found."
